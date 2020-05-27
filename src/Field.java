b0a9aa2515a77f86a88f47f457352de0f7f2bce6
Waqas Rehmani <dada.scene@hotmail.com>
2020-05-27 12:29:14 +1000

    Field Class

---
+++ src/Field.java
@@ -0,0 +1,199 @@
+/**
+ *  Field.java
+ *
+ *  This is the Field class that represents the Field in the simulator.
+ *  It is a PROCESS as it extends Thread.
+ *
+ *  ============================================================================
+ *  @authorName     : Waqas Rehmani, Angus Hudson, Jonathan Dunne
+ *  @studentNumber  : 1035514, 835808, 836748
+ *  ============================================================================
+ *
+ *  ============================================================================
+ *  Description of the object properties:
+ *  ============================================================================
+ *
+ *
+ *  ============================================================================
+ *  Detailed description of Methods:
+ *  ============================================================================
+ *      
+ * 
+ */
+
+
+public class Field {
+    // =========================================================================
+    // Class Properties
+    // =========================================================================
+    private int width, height;
+    Patch[][] patches;
+
+    // =========================================================================
+    // Constructor: Creates a new patch for the given values for grain
+    // and max grain.
+    // =========================================================================
+    Field (int width, int height) {
+        // Initialising all the properties here.
+        this.width  = width;
+        this.height = height; 
+        this.patches = new Patch[Params.FIELD_HEIGHT][Params.FIELD_WIDTH];
+        initialisePatches();
+    }
+
+    // =========================================================================
+    // Method: Replenish the amount of grain on the patch
+    // =========================================================================
+    private void initialisePatches() {
+        // Initialise patches to be empty with 0 maxin, unless they are 'rich'
+        int maxGrain;
+        int nGrain;
+        for (int i = 0; i < Params.FIELD_HEIGHT; i++) {
+            for (int j = 0; j < Params.FIELD_WIDTH; j++) {
+                maxGrain = nGrain = 0;
+                if (Params.getRandomPercentage() <= Params.PERCENT_BEST_LAND) {
+                    maxGrain = nGrain = Params.MAX_GRAIN; // 'Rich' patches
+                }
+                this.patches[i][j] = new Patch(nGrain, maxGrain, i, j);
+            }
+        }
+
+        printGrain(this.patches);
+
+        for (int i = 0; i < Params.N_FIRST_DIFFUSIONS; i++) {
+            printIterationHeader(i+1);
+            fillRichPatches();
+            printGrain(this.patches);
+            diffusePatches();
+            printGrain(this.patches);
+        }
+
+        for (int i = 0; i < Params.N_SECOND_DIFFUSIONS; i++) {
+            printIterationHeader(Params.N_FIRST_DIFFUSIONS + i + 1);
+            diffusePatches();
+            printGrain(this.patches);
+        }
+
+        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+                this.patches[row][col].setCurrGrain((int) this.patches[row][col].getCurrGrain());
+                this.patches[row][col].setMaxGrain((int) this.patches[row][col].getCurrGrain());
+                printPatch(this.patches[row][col], row+1, col+1);
+            }
+        }
+    }
+
+    // =========================================================================
+    // Method: The diffusion of patches.
+    // =========================================================================
+    public void diffusePatches() {
+        // We use a separate grid so the patches aren't updated until ALL have been processed for diffusion
+        double[][] transitionGrid = new double[Params.FIELD_HEIGHT][Params.FIELD_WIDTH];
+        int left, right, up, down;
+        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+                up = row - 1;
+                down = row + 1;
+                left = col - 1;
+                right = col + 1;
+                // The board wraps horizontally and vertically
+                if (up < 0) {
+                    up = Params.FIELD_HEIGHT - 1;
+                }
+                if (down == Params.FIELD_HEIGHT) {
+                    down = 0;
+                }
+                if (left < 0) {
+                    left = Params.FIELD_WIDTH - 1;
+                }
+                if (right == Params.FIELD_WIDTH) {
+                    right = 0;
+                }
+                // Now update all transitions
+                double diffuse = Params.DIFFUSION_RATE * (1.0/8) * this.patches[row][col].getCurrGrain();
+
+                transitionGrid[up][left] += diffuse;
+                transitionGrid[up][col] += diffuse;
+                transitionGrid[up][right] += diffuse;
+
+                transitionGrid[row][left] += diffuse;
+                transitionGrid[row][right] += diffuse;
+
+                transitionGrid[down][left] += diffuse;
+                transitionGrid[down][col] += diffuse;
+                transitionGrid[down][right] += diffuse;
+
+                transitionGrid[row][col] -= 8 * diffuse;
+            }
+        }
+
+        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+                this.patches[row][col].setCurrGrain(this.patches[row][col].getCurrGrain() + transitionGrid[row][col]);
+            }
+        }
+    }
+
+    public void fillRichPatches() {
+        for (int row = 0; row < Params.FIELD_WIDTH; row++) {
+            for (int col = 0; col < Params.FIELD_HEIGHT; col++) {
+                if (this.patches[row][col].getMaxGrain() != 0) {
+                    this.patches[row][col].setCurrGrain(this.patches[row][col].getMaxGrain());;
+                }
+            }
+        }
+    }
+
+    public void printGrain(Patch[][] patches) {
+        String s;
+        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+                s = String.format("%5.2f ", patches[row][col].getCurrGrain());
+                System.out.print(s);
+                if (col == Params.FIELD_WIDTH-1) {
+                    System.out.print("\n");
+                }
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public void printIterationHeader(int x) {
+        String s;
+        s = String.format("=== ITERATION %d ===\n\n", x);
+        System.out.print(s);
+    }
+
+    public void printPatch(Patch p, int row, int col) {
+        String s;
+        s = String.format("PATCH Row: %2d Col: %2d\n" +
+                "Curr Grain:\t\t%3.1f\nMax Grain:\t\t%3d\n\n", row, col,
+                p.getCurrGrain(), p.getMaxGrain());
+        System.out.print(s);
+    }
+
+    public void growPatches() {
+        for (Patch[] patchRow : this.patches) {
+            for (Patch p : patchRow) {
+                p.grow();
+            }
+        }
+    }
+
+    // =========================================================================
+    // Getters and Setters
+    // =========================================================================
+
+    public Patch getPatch(int row, int col) {
+        // not sure if I exchanged row and col??
+        return this.patches[row][height];
+    }
+
+    public Patch getRandomPatch() {
+        int patchRow = Params.getRandomRow();
+        int patchCol = Params.getRandomCol();
+        return this.patches[patchRow][patchCol];
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -27,14 +27,15 @@ public class Main {
 
     public static void main(String [] args) throws InterruptedException {
 
-        Patch[][] patches = new Patch[Params.FIELD_HEIGHT][Params.FIELD_WIDTH];
+        // Patch[][] patches = new Patch[Params.FIELD_HEIGHT][Params.FIELD_WIDTH];
         Turtle[] turtles = new Turtle[Params.NUM_TURTLES];
+        Field field = new Field(Params.FIELD_WIDTH, Params.FIELD_HEIGHT);
 
-        initialisePatches(patches);
+        // initialisePatches(patches);
 
-        initialiseTurtles(turtles, patches);
+        initialiseTurtles(turtles, field);
 
-        runSimulation(patches, turtles);
+        runSimulation(turtles, field);
 
         /*
         List<List<String>> rows = Arrays.asList(
@@ -69,53 +70,54 @@ public class Main {
         }
     }
 
-    public static void initialisePatches(Patch[][] patches) {
-        // Initialise patches to be empty with 0 maxin, unless they are 'rich'
-        int maxGrain;
-        int nGrain;
-        for (int i = 0; i < Params.FIELD_HEIGHT; i++) {
-            for (int j = 0; j < Params.FIELD_WIDTH; j++) {
-                maxGrain = nGrain = 0;
-                if (Params.getRandomPercentage() <= Params.PERCENT_BEST_LAND) {
-                    maxGrain = nGrain = Params.MAX_GRAIN; // 'Rich' patches
-                }
-                patches[i][j] = new Patch(nGrain, maxGrain, i, j);
-            }
-        }
+    // public static void initialisePatches(Patch[][] patches) {
+    //     // Initialise patches to be empty with 0 maxin, unless they are 'rich'
+    //     int maxGrain;
+    //     int nGrain;
+    //     for (int i = 0; i < Params.FIELD_HEIGHT; i++) {
+    //         for (int j = 0; j < Params.FIELD_WIDTH; j++) {
+    //             maxGrain = nGrain = 0;
+    //             if (Params.getRandomPercentage() <= Params.PERCENT_BEST_LAND) {
+    //                 maxGrain = nGrain = Params.MAX_GRAIN; // 'Rich' patches
+    //             }
+    //             patches[i][j] = new Patch(nGrain, maxGrain, i, j);
+    //         }
+    //     }
 
-        printGrain(patches);
+    //     printGrain(patches);
 
-        for (int i = 0; i < Params.N_FIRST_DIFFUSIONS; i++) {
-            printIterationHeader(i+1);
-            fillRichPatches(patches);
-            printGrain(patches);
-            diffusePatches(patches);
-            printGrain(patches);
-        }
+    //     for (int i = 0; i < Params.N_FIRST_DIFFUSIONS; i++) {
+    //         printIterationHeader(i+1);
+    //         fillRichPatches(patches);
+    //         printGrain(patches);
+    //         diffusePatches(patches);
+    //         printGrain(patches);
+    //     }
 
-        for (int i = 0; i < Params.N_SECOND_DIFFUSIONS; i++) {
-            printIterationHeader(Params.N_FIRST_DIFFUSIONS + i + 1);
-            diffusePatches(patches);
-            printGrain(patches);
-        }
+    //     for (int i = 0; i < Params.N_SECOND_DIFFUSIONS; i++) {
+    //         printIterationHeader(Params.N_FIRST_DIFFUSIONS + i + 1);
+    //         diffusePatches(patches);
+    //         printGrain(patches);
+    //     }
 
-        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
-            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
-                patches[row][col].setCurrGrain((int) patches[row][col].getCurrGrain());
-                patches[row][col].setMaxGrain((int) patches[row][col].getCurrGrain());
-                printPatch(patches[row][col], row+1, col+1);
-            }
-        }
-    }
+    //     for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+    //         for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+    //             patches[row][col].setCurrGrain((int) patches[row][col].getCurrGrain());
+    //             patches[row][col].setMaxGrain((int) patches[row][col].getCurrGrain());
+    //             printPatch(patches[row][col], row+1, col+1);
+    //         }
+    //     }
+    // }
 
-    public static void initialiseTurtles(Turtle[] turtles, Patch[][] patches) {
+    public static void initialiseTurtles(Turtle[] turtles, Field field) {
         for (int i = 0; i < Params.NUM_TURTLES; i++) {
             
             // set turtle initial location
-            int patchRow = Params.getRandomRow();
-            int patchWidth = Params.getRandomCol();
+            // int patchRow = Params.getRandomRow();
+            // int patchWidth = Params.getRandomCol();
+            Patch patch = field.getRandomPatch();
             
-            turtles[i] = initialiseTurtle(patches[patchRow][patchWidth]);
+            turtles[i] = initialiseTurtle(patch);
         }
 
         recolorTurtles(turtles);
@@ -140,91 +142,91 @@ public class Main {
     // 2. Do this 5 times
     // 3. Do this a further 10 times, except we don't replenish the 'rich' patches
     // 4. After, round all grain values for each patch down to nearest int, and set max for that patch to be that value
-    public static void diffusePatches(Patch[][] patches) {
-        // We use a separate grid so the patches aren't updated until ALL have been processed for diffusion
-        double[][] transitionGrid = new double[Params.FIELD_HEIGHT][Params.FIELD_WIDTH];
-        int left, right, up, down;
-        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
-            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
-                up = row - 1;
-                down = row + 1;
-                left = col - 1;
-                right = col + 1;
-                // The board wraps horizontally and vertically
-                if (up < 0) {
-                    up = Params.FIELD_HEIGHT - 1;
-                }
-                if (down == Params.FIELD_HEIGHT) {
-                    down = 0;
-                }
-                if (left < 0) {
-                    left = Params.FIELD_WIDTH - 1;
-                }
-                if (right == Params.FIELD_WIDTH) {
-                    right = 0;
-                }
-                // Now update all transitions
-                double diffuse = Params.DIFFUSION_RATE * (1.0/8) * patches[row][col].getCurrGrain();
+    // public static void diffusePatches(Patch[][] patches) {
+    //     // We use a separate grid so the patches aren't updated until ALL have been processed for diffusion
+    //     double[][] transitionGrid = new double[Params.FIELD_HEIGHT][Params.FIELD_WIDTH];
+    //     int left, right, up, down;
+    //     for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+    //         for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+    //             up = row - 1;
+    //             down = row + 1;
+    //             left = col - 1;
+    //             right = col + 1;
+    //             // The board wraps horizontally and vertically
+    //             if (up < 0) {
+    //                 up = Params.FIELD_HEIGHT - 1;
+    //             }
+    //             if (down == Params.FIELD_HEIGHT) {
+    //                 down = 0;
+    //             }
+    //             if (left < 0) {
+    //                 left = Params.FIELD_WIDTH - 1;
+    //             }
+    //             if (right == Params.FIELD_WIDTH) {
+    //                 right = 0;
+    //             }
+    //             // Now update all transitions
+    //             double diffuse = Params.DIFFUSION_RATE * (1.0/8) * patches[row][col].getCurrGrain();
 
-                transitionGrid[up][left] += diffuse;
-                transitionGrid[up][col] += diffuse;
-                transitionGrid[up][right] += diffuse;
+    //             transitionGrid[up][left] += diffuse;
+    //             transitionGrid[up][col] += diffuse;
+    //             transitionGrid[up][right] += diffuse;
 
-                transitionGrid[row][left] += diffuse;
-                transitionGrid[row][right] += diffuse;
+    //             transitionGrid[row][left] += diffuse;
+    //             transitionGrid[row][right] += diffuse;
 
-                transitionGrid[down][left] += diffuse;
-                transitionGrid[down][col] += diffuse;
-                transitionGrid[down][right] += diffuse;
+    //             transitionGrid[down][left] += diffuse;
+    //             transitionGrid[down][col] += diffuse;
+    //             transitionGrid[down][right] += diffuse;
 
-                transitionGrid[row][col] -= 8 * diffuse;
-            }
-        }
+    //             transitionGrid[row][col] -= 8 * diffuse;
+    //         }
+    //     }
 
-        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
-            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
-                patches[row][col].setCurrGrain(patches[row][col].getCurrGrain() + transitionGrid[row][col]);
-            }
-        }
-    }
+    //     for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+    //         for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+    //             patches[row][col].setCurrGrain(patches[row][col].getCurrGrain() + transitionGrid[row][col]);
+    //         }
+    //     }
+    // }
 
-    public static void fillRichPatches(Patch[][] patches) {
-        for (int row = 0; row < Params.FIELD_WIDTH; row++) {
-            for (int col = 0; col < Params.FIELD_HEIGHT; col++) {
-                if (patches[row][col].getMaxGrain() != 0) {
-                    patches[row][col].setCurrGrain(patches[row][col].getMaxGrain());;
-                }
-            }
-        }
-    }
+    // public static void fillRichPatches(Patch[][] patches) {
+    //     for (int row = 0; row < Params.FIELD_WIDTH; row++) {
+    //         for (int col = 0; col < Params.FIELD_HEIGHT; col++) {
+    //             if (patches[row][col].getMaxGrain() != 0) {
+    //                 patches[row][col].setCurrGrain(patches[row][col].getMaxGrain());;
+    //             }
+    //         }
+    //     }
+    // }
 
-    public static void printGrain(Patch[][] patches) {
-        String s;
-        for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
-            for (int col = 0; col < Params.FIELD_WIDTH; col++) {
-                s = String.format("%5.2f ", patches[row][col].getCurrGrain());
-                System.out.print(s);
-                if (col == Params.FIELD_WIDTH-1) {
-                    System.out.print("\n");
-                }
-            }
-        }
-        System.out.print("\n");
-    }
+    // public static void printGrain(Patch[][] patches) {
+    //     String s;
+    //     for (int row = 0; row < Params.FIELD_HEIGHT; row++) {
+    //         for (int col = 0; col < Params.FIELD_WIDTH; col++) {
+    //             s = String.format("%5.2f ", patches[row][col].getCurrGrain());
+    //             System.out.print(s);
+    //             if (col == Params.FIELD_WIDTH-1) {
+    //                 System.out.print("\n");
+    //             }
+    //         }
+    //     }
+    //     System.out.print("\n");
+    // }
 
-    public static void printIterationHeader(int x) {
-        String s;
-        s = String.format("=== ITERATION %d ===\n\n", x);
-        System.out.print(s);
-    }
+    // public static void printIterationHeader(int x) {
+    //     String s;
+    //     s = String.format("=== ITERATION %d ===\n\n", x);
+    //     System.out.print(s);
+    // }
 
-    public static void printPatch(Patch p, int row, int col) {
-        String s;
-        s = String.format("PATCH Row: %2d Col: %2d\n" +
-                "Curr Grain:\t\t%3.1f\nMax Grain:\t\t%3d\n\n", row, col,
-                p.getCurrGrain(), p.getMaxGrain());
-        System.out.print(s);
-    }
+    // public static void printPatch(Patch p, int row, int col) {
+    //     String s;
+    //     s = String.format("PATCH Row: %2d Col: %2d\n" +
+    //             "Curr Grain:\t\t%3.1f\nMax Grain:\t\t%3d\n\n", row, col,
+    //             p.getCurrGrain(), p.getMaxGrain());
+    //     System.out.print(s);
+    // }
 
     public static void printDistribution(Turtle[] turtles) {
         String s;
@@ -257,29 +259,30 @@ public class Main {
         }
     }
 
-    public static void runSimulation(Patch[][] patches, Turtle[] turtles) {
+    public static void runSimulation(Turtle[] turtles, Field field) {
         int ticks = 0;
         while (ticks < Params.N_TICKS) {
             for (Turtle t : turtles) {
-                turnTowardsGrain(t, patches);
+                turnTowardsGrain(t, field);
             }
             for (Turtle t : turtles) {
                 t.harvest();
             }
             // harvest(turtles);
             for (Turtle t : turtles) {
-                moveEatAgeDie(t, patches);
+                moveEatAgeDie(t, field);
             }
             recolorTurtles(turtles);
             printDistribution(turtles);
 
             if (ticks % Params.GRAIN_GROWTH_INTERVAL == 0) {
-                for (Patch[] patchRow : patches) {
-                    for (Patch p : patchRow) {
-                        p.grow();
-                        // growGrain(p);
-                    }
-                }
+                field.growPatches();
+                // for (Patch[] patchRow : patches) {
+                //     for (Patch p : patchRow) {
+                //         p.grow();
+                //         // growGrain(p);
+                //     }
+                // }
             }
             // updateLorenzAndGini();
             ticks++;
@@ -289,25 +292,25 @@ public class Main {
     // =========================================================================
     // Method: To turn the Turtle towards the grain.
     // =========================================================================
-    public static void turnTowardsGrain(Turtle t, Patch[][] patches) {
+    public static void turnTowardsGrain(Turtle t, Field field) {
         t.setHeading(0);
         int bestDirection = 0;
-        int bestAmount = grainAhead(t, patches);
+        int bestAmount = grainAhead(t, field);
 
         t.setHeading(90);
-        if (grainAhead(t, patches) > bestAmount) {
+        if (grainAhead(t, field) > bestAmount) {
             bestDirection = 90;
-            bestAmount = grainAhead(t, patches);
+            bestAmount = grainAhead(t, field);
         }
 
         t.setHeading(180);
-        if (grainAhead(t, patches) > bestAmount) {
+        if (grainAhead(t, field) > bestAmount) {
             bestDirection = 180;
-            bestAmount = grainAhead(t, patches);
+            bestAmount = grainAhead(t, field);
         }
 
         t.setHeading(270);
-        if (grainAhead(t, patches) > bestAmount) {
+        if (grainAhead(t, field) > bestAmount) {
             bestDirection = 270;
         }
 
@@ -318,9 +321,9 @@ public class Main {
     // =========================================================================
     // Method: To check the grains in the Patches ahead.
     // =========================================================================
-    public static int grainAhead(Turtle t, Patch[][] patches) {
+    public static int grainAhead(Turtle t, Field field) {
         int tPatchRow = t.getCurrLocation().getRow();
-        int tPatchCol = t.getCurrLocation().getCol();
+        int tPatchCol = t.getCurrLocation().getCol(); 
         int tHeading = t.getHeading();
         int grainTotal = 0;
         for (int i = 0; i < t.getVision(); i++) {
@@ -350,7 +353,7 @@ public class Main {
             if (tPatchRow == Params.FIELD_WIDTH) {
                 tPatchRow = 0;
             }
-            grainTotal += patches[tPatchRow][tPatchCol].getCurrGrain();
+            grainTotal += field.getPatch(tPatchRow, tPatchCol).getCurrGrain();
         }
         return grainTotal;
     }
@@ -369,7 +372,7 @@ public class Main {
     //     }
     // }
 
-    public static void moveEatAgeDie(Turtle t, Patch[][] patches) {
+    public static void moveEatAgeDie(Turtle t, Field field) {
         // move turtle one step in the direction it's facing.
         // todo: abstract this code to seperate function (it's ripped from 
         // grainsAhead lmao)
@@ -408,7 +411,7 @@ public class Main {
         }
 
         // move turtle
-        t.setCurrLocation(patches[tPatchRow][tPatchCol]);
+        t.setCurrLocation(field.getPatch(tPatchRow, tPatchCol));
 
         // consume some grain according to metabolism
         t.setWealth(t.getWealth() - t.getMetabolism());
